3ca7b9d2303ec9317d98bd8f9b1efbddee35bf84
hongli chen <zerochl0912@gmail.com>
2018-01-23 22:16:49 +0800

    修改配置文件，桌面产生图标

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -466,6 +466,18 @@ public class Launcher extends BaseActivity
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onCreate(savedInstanceState);
         }
+//        Context c =  this;
+//        PackageManager p = c.getPackageManager();
+//
+//        ComponentName cN = new ComponentName(c, Launcher.class);
+//        p.setComponentEnabledSetting(cN, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+//
+//        Intent selector = new Intent(Intent.ACTION_MAIN);
+//        selector.addCategory(Intent.CATEGORY_HOME);
+//        c.startActivity(selector);
+//
+//        p.setComponentEnabledSetting(cN, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+
     }
 
     @Override
